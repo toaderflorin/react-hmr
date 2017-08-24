b476ab6d7ae57bd3a15804a70e1645c7a56fd989
toaderflorin <toaderflorin@gmail.com>
2017-08-24 09:29:52 +0300

    Added Babel.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello')
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+const start = async () => {
+  app.use(express.static(__dirname + '/content'))
+  app.listen(3000, () => console.log('Started successfully, open localhost:3000.'))
+
+  app.get('/', (req, res) => {
+    console.log('got here')
+    var contents = fs.readFileSync('content/index.html', 'utf8');
+    console.log(contents);
+    res.send('heloooo')
+  })
+}
+
+start()
