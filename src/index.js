4fe16d6aa663fd5bf351224fa8d5d7a92bc6e179
toaderflorin <toaderflorin@gmail.com>
2017-08-24 13:54:16 +0300

    Added webpack and removed Babel.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ const app = express()
 const fs = require('fs')
 
 const start = async () => {
-  app.use(express.static(__dirname + '/content'))
+  app.use(express.static(__dirname + '/static'))
   app.listen(3000, () => console.log('Started successfully, open localhost:3000.'))
 
   app.get('/', (req, res) => {
     console.log('got here')
-    var contents = fs.readFileSync('content/index.html', 'utf8');
-    console.log(contents);
+    var contents = fs.readFileSync('content/index.html', 'utf8')    
     res.send('heloooo')
   })
 }
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// module.exports = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve({
+//         entry: './src/static/app/file.js'        
+//       })
+//     }, 5000)
+//   })
+// }
